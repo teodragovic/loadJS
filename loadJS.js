3c12bc8b2f1800dbc386c8d1b2d6af89dae6d4f1
Teo Dragovic <teo.dragovic@infobip.com>
2015-07-08 16:01:17 +0200

    Add support for AMD and Node

--- loadJS.js
+++ loadJS.js
@@ -1,13 +1,32 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
-function loadJS( src, cb ){
+(function () {
 	"use strict";
-	var ref = window.document.getElementsByTagName( "script" )[ 0 ];
-	var script = window.document.createElement( "script" );
-	script.src = src;
-	script.async = true;
-	ref.parentNode.insertBefore( script, ref );
-	if (cb && typeof(cb) === "function") {
-		script.onload = cb;
+
+	function loadJS( src, cb ){
+		var ref = window.document.getElementsByTagName( "script" )[ 0 ];
+		var script = window.document.createElement( "script" );
+		script.src = src;
+		script.async = true;
+		ref.parentNode.insertBefore( script, ref );
+		if (cb && typeof(cb) === "function") {
+			script.onload = cb;
+		}
+		return script;
 	}
-	return script;
-}
+
+	if (typeof define === "function" && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(function () {
+			return loadJS;
+		});
+	} else if (typeof exports === "object") {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = loadJS;
+	} else {
+		// Browser globals
+		window.loadJS = loadJS;
+	}
+
+})();
